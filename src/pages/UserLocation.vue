52c61a58cc9575f5a0d80b47bfbcef59d4d93f5a
salexfarm <safarmer@me.com>
2023-01-11 22:23:31 +0000

    007: Build user location form using Semantic-UI

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -1,6 +1,19 @@
 <template>
-  <section class="ui two column grid centered grid">
-    <div class="column red"></div>
+  <section class="ui two column centered grid">
+    <div class="column">
+      <form class="ui segment large form">
+        <div class="ui message red"></div>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui right icon input large">
+              <input type="text" placeholder="Enter your address"/>
+              <i class="dot circle link icon"></i>
+            </div>
+          </div>
+          <button class="ui button pink">Go</button>
+        </div>
+      </form>
+    </div>
   </section>
 </template>
 
